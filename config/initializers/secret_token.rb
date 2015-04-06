bac3d35c2bae22f5b71805263c0268209c2061b2
brokenfingers <hyunsikk91@gmail.com>
2015-04-05 23:15:22 -0700

    Skeleton Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FabricManager::Application.config.secret_token = 'cbbab473e6d62798bf28650b6e26f15302fd951670390cb6e8e6b57b697cb7ea7c53c95ffb676196b9d77a1f9f49ed4ce16f7326a14e46f8bcb17777e5dc5f86'

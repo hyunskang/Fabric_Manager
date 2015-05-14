31848c012ce9294d579212595d76e53558195e8b
brokenfingers <hyunsikk91@gmail.com>
2015-05-14 05:43:49 -0700

    Added Jasmine test for the js

--- app/assets/javascripts/create_fabrics.js
+++ app/assets/javascripts/create_fabrics.js
@@ -1,10 +1,10 @@
 var FabricCreation = {
   setup: function() {
-    $('.fabric_creation_form').hide();
-    $(document).on('click', '.create_button', FabricCreation.display_form);
+    $('#fabric_creation_form').hide();
+    $(document).on('click', '#create_button', FabricCreation.display_form);
   },
   display_form: function() {
-    $('.fabric_creation_form').show();
+    $('#fabric_creation_form').show();
     $(document).on('click', '#add_fabric_btn', FabricCreation.setup);
   }
 };

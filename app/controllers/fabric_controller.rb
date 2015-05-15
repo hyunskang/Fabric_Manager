888e0a33c14e139c65a72ec0208a4a3f2d4b4508
brokenfingers <hyunsikk91@gmail.com>
2015-05-15 00:08:03 -0700

    Added fabric creation input validation

--- app/controllers/fabric_controller.rb
+++ app/controllers/fabric_controller.rb
@@ -2,7 +2,10 @@ class FabricController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @fabric = Fabric.new(params[:fabric])
+    # Validate the input passed to the controller action
+    if validate_attr(params[:fabric])
+      @fabric = Fabric.new(params[:fabric])
+    end
     if @fabric.save
       respond_to do |format|
         format.js { render 'create.js.erb' }
@@ -11,5 +14,22 @@ class FabricController < ApplicationController
     else
       render :nothing => true
     end
+  end
+
+  # Takes a hash as parameter and returns true or false
+  def validate_attr(fab_attr)
+    quantity = Integer(fab_attr[:quantity]) rescue false
+    price = Float(fab_attr[:price]) rescue false
+    if fab_attr[:serial].length < 10 || !/\W/.match(fab_attr[:serial]).nil?
+      flash[:alert] = "Enter a valid alphanumeric serial number."
+      return false
+    elsif !/\W|\d/.match(fab_attr[:color]).nil?
+      flash[:alert] = "Enter a valid color."
+      return false
+    elsif !quantity || !price
+      flash[:alert] = "Enter a valid quantity or price."
+      return false
+    end
+    return true
   end
 end
--- spec/controllers/fabric_controller_spec.rb
+++ spec/controllers/fabric_controller_spec.rb
@@ -13,14 +13,71 @@ describe FabricController do
     end
 
     it "response should be success" do
+      FabricController.any_instance.stub(:validate_attr).and_return(true)
       post 'create', :user_id => 1, :fabric => @fabric_param
       response.should be_success
     end
 
     it "should create a new fabric record" do 
       expect{
+        FabricController.any_instance.stub(:validate_attr).and_return(true)
         post 'create', :user_id => 1, :fabric => @fabric_param
         }.to change{Fabric.count}.by(1)
     end
+  end
+
+  describe "validate input" do
+    before :each do
+      @test_params = {
+        color: "Yellow",
+        serial: "AMMEL123EEA",
+        quantity: 200,
+        price: 2.44
+      }
+    end
+
+    it "should detect invalid character in serial number" do
+      @test_params[:serial] = "@@@@"
+      controller.validate_attr(@test_params).should == false
+    end
+
+    it "should detect a serial number with less than 10 characters" do
+      @test_params[:serial] = "A"
+      controller.validate_attr(@test_params).should == false
+    end
+
+    it "should assign the correct serial error message" do
+      @test_params[:serial] = "A"
+      controller.validate_attr(@test_params)
+      flash[:alert].should == "Enter a valid alphanumeric serial number."
+    end
+
+    it "should detect invalid character in color" do
+      @test_params[:color] = "22@11"
+      controller.validate_attr(@test_params).should == false
+    end
+
+    it "should assign the correct color error message" do
+      @test_params[:color] = "22@11"
+      controller.validate_attr(@test_params)
+      flash[:alert].should == "Enter a valid color."
+    end
+
+    it "should detect invalid quantity input" do
+      @test_params[:quantity] = "EE"
+      controller.validate_attr(@test_params).should == false
+    end
+
+    it "should detect invalid price input" do
+      @test_params[:price] = "@@"
+      controller.validate_attr(@test_params).should == false
+    end
+
+    it "should assign the correct price/quantity error message" do 
+      @test_params[:price] = "@@"
+      @test_params[:quantity] = "A"
+      controller.validate_attr(@test_params)
+      flash[:alert].should == "Enter a valid quantity or price."
+    end 
   end
 end

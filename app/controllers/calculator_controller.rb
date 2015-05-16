958b1eb08ba735888a17c302ca2e1340f9a15463
brokenfingers <hyunsikk91@gmail.com>
2015-05-16 05:15:03 -0700

    Added show for fabric

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,9 @@
 class CalculatorController < ApplicationController
   before_filter :authenticate_user!
 
+  def calculate
+    # Should calculate the total cost from the meters received and update
+    # the total meters sold and total profit
+    # Future updates: should update the daily records
+  end
 end

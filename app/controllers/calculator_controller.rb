abce4dd132593a22f0b55aa0201b28e30fa8cf66
brokenfingers <hyunsikk91@gmail.com>
2015-05-20 06:30:23 -0700

    Transaction record creating logic

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -5,13 +5,25 @@ class CalculatorController < ApplicationController
     # Should calculate the total cost from the meters received and update
     # the total meters sold and total profit
     # Future updates: should update the daily records
-    input_meters = BigDecimal.new(params[:meters])
     @fabric = Fabric.where("id=?", params[:fabric_id]).first
-    @fabric.update_attributes({meters_sold: input_meters + @fabric.meters_sold, total_profit: input_meters*@fabric.price + @fabric.total_profit})
-    # @fabric.add_new_record()
+    data_hash = parse_data_from_params(@fabric)
+    @fabric.update_attributes({meters_sold: data_hash[:meters_sold], total_profit: data_hash[:profit]})
+    @fabric.add_new_record(data_hash)
     respond_to do |format|
       format.js
       format.html {redirect_to user_fabric_path(params[:user_id], params[:fabric_id])}
     end
+  end
+
+  def parse_data_from_params(fabric)
+    input_meters = BigDecimal.new(params[:meters])
+    data = Hash.new
+    data[:meters_sold] = input_meters + fabric.meters_sold
+    data[:profit] = input_meters*fabric.price + fabric.total_profit
+    data[:price] = params[:fabric][:price]
+    data[:transaction_year] = params["transaction_date(1i)"]
+    data[:transaction_month] = params["transaction_date(2i)"]
+    data[:transaction_day] = params["transaction_date(3i)"]
+    data
   end
 end
